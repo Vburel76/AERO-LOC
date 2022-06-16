3c0ebf6a571ec15d359c7d721412acafeebd69e7
Vburel76 <valentin.burel.76600@gmail.com>
2022-06-16 17:15:24 +0200

    navBar creation en cours

---
+++ views/landing.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS only -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../public/css/style.css">
+
+    <title>Aero-Loc</title>
+</head>
+
+<body>
+
+    <header class="text-center m-3">
+        <h1>Aero-Loc</h1>
+    </header>
+
+
+
+    <nav class="navbar navbar-expand-lg bg-light border border-dark justify-content-around">
+
+        <div class="container-fluid">
+
+           
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="d-lg-none d-block">
+                <i class="bi bi-person-circle taille"></i>
+            </div>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item ">
+                        <a class="nav-link fs-3 textNavbar " href="#">A propos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-3 textNavbar " href="#">La flotte</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-3 textNavbar " href="#">Formation</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-3 textNavbar " href="#">Location</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-3 textNavbar " href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="d-lg-block d-none">
+                <i class="bi bi-person-circle taille"></i>
+            </div>
+
+        </div>
+
+    </nav>
+
+
+
+
+    <main>
+    </main>
+    <footer>
+    </footer>
+
+
+
+
+
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+
+</html>
