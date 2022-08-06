b92971e8cf29a6f6cb2dc9a72bc238fbb9df1de5
Vburel76 <valentin.burel.76600@gmail.com>
2022-08-06 11:58:15 +0200

    formulaire

--- views/formation.php
+++ views/formation.php
@@ -3,67 +3,63 @@
 <body>
     <?php include '../elements/navBar.php' ?>
 
-    <h1 class="text-center mt-4">Les formations</h1>
+    <h1 class="text-center mt-4 textColor2 fontTitle">Les formations</h1>
 
     <div class="row justify-content-center formationBackground m-0 p-0">
         <div class="col-lg-8 col-12 text-light">
-            <p class="text-center fs-3">Quesque le PPL ?</p>
+            <p class="text-center fs-3 fontTitle">Quesque le PPL ?</p>
             <br>
-            <p class="fs-4">La licence PPL (Private Pilot Licence) est une formation récente accessible dès 45 heures de vol.
+            <p class="fs-4 fontText">La licence PPL (Private Pilot Licence) est une formation récente accessible dès 45 heures de vol.
                 Cette licence européenne confère à son détenteur les mêmes privilèges que la LAPL sans la limitation du nombre de passagers et sans la limitation de la masse de l’appareil (>2 tonnes).
                 C’est aujourd’hui la licence de pilote à caractère non professionnel la plus répandue.
                 La PPL(A) est une licence européenne qui vous permet d’évoluer vers d’autres qualifications.
                 La formation de pilote privé est dispensée par un instructeur qualifié.</p>
 
-            <p class="fs-4">Durée de la formation</p>
+            <p class="fs-4 fontTitle">Durée de la formation</p>
 
-            <p class="fs-4">La formation se déroule sur une période de 6 à 18 mois en fonction des disponibilités et du temps que vous y consacrez.Le programme est adapté en fonction de la rapidité d'apprenstissage du client.</p>
+            <p class="fs-4 fontText">La formation se déroule sur une période de 6 à 18 mois en fonction des disponibilités et du temps que vous y consacrez.Le programme est adapté en fonction de la rapidité d'apprenstissage du client.</p>
         </div>
     </div>
 
 
 
 
-    <p class="fs-3 text-center mt-4">Conditions d'admission pour la formation</p>
+    <p class="fs-3 text-center mt-4 textColor2 fontTitle">Conditions d'admission pour la formation</p>
 
-    <p class="text-center mt-4">Un certain nombres de conditions doivent etre remplir pour pouvoir commencer la formation.</p>
+    <p class="text-center mt-4 fontText">Un certain nombres de conditions doivent etre remplir pour pouvoir commencer la formation.</p>
 
     <div class="row justify-content-evenly mt-5 m-0 p-0">
-        <div class="col-lg-3 text-center">
-            <p class="border border-dark"><i class="bi bi-globe fs-4 m-2"></i>être francais</p>
+        <div class="col-lg-3 text-center Textcolor">
+            <p class=" m-2 fontText"><i class="bi bi-globe fs-4 m-2"></i>être francais</p>
         </div>
-        <div class="col-lg-3 text-center">
-            <p class="border border-dark"><i class="bi bi-check2-circle fs-4 m-2"></i>avoir l'age minimum requis</p>
+        <div class="col-lg-3 text-center Textcolor">
+            <p class=" m-2 fontText"><i class="bi bi-check2-circle fs-4 m-2"></i>avoir l'age minimum requis</p>
         </div>
-        <div class="col-lg-3 text-center">
-            <p class="border border-dark"><i class="bi bi-heart-pulse fs-4 m-2"></i>être apte médicalement</p>
+        <div class="col-lg-3 text-center Textcolor">
+            <p class=" m-2 fontText"><i class="bi bi-heart-pulse fs-4 m-2"></i>être apte médicalement</p>
         </div>
     </div>
 
-
-
-    <div class="row m-0 p-0">
-        (image)
-    </div>
 
     <div class="row justify-content-center m-0 p-0">
         <div class="col-lg-10 mt-5 ">
-            <p class="fs-4 text-center">Déroulé de la formation</p>
-            <p>La formation commence par une partie théorique de plusieurs heures et qui s'effectue dans une salle de cours avec un formateur assermenté, cette partie theorique fera l'objet d'une évaluation.Par la suite la partie pratique se déroulare tous d'abord en simulateur et ensuite en avion, la aussi vous passerez une évaluation qui determinera l'obtention du PPL ou pas . </p>
+            <p class="fs-3 text-center textColor2 fontTitle">Déroulé de la formation</p>
+
+            <p class="fontText">La formation commence par une partie théorique de plusieurs heures et qui s'effectue dans une salle de cours avec un formateur assermenté, cette partie theorique fera l'objet d'une évaluation.Par la suite la partie pratique se déroulare tous d'abord en simulateur et ensuite en avion, la aussi vous passerez une évaluation qui determinera l'obtention du PPL ou pas . </p>
 
         </div>
     </div>
 
-    <p class="text-center mt-5 fs-4">Programme de la formation théorique</p>
+    <p class="text-center mt-5 fs-4 textColor2 fontTitle">Programme de la formation théorique</p>
 
     <div class="row m-0 p-0 justify-content-center">
-        <div class="col-lg-8">
+        <div class="col-lg-8 borderColor">
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col" class="tableauSize">Module</th>
-                        <th scope="col" class="tableauSize">Sujet</th>
-                        <th scope="col" class="tableauSize">Temps</th>
+                        <th scope="col" class="tableauSize fontText">Module</th>
+                        <th scope="col" class="tableauSize fontText">Sujet</th>
+                        <th scope="col" class="tableauSize fontText">Temps</th>
                     </tr>
                 </thead>
                 <tbody class="table-group-divider">
@@ -93,9 +89,9 @@
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col" class="tableauSize">Module</th>
-                        <th scope="col" class="tableauSize">Sujet ciblé</th>
-                        <th scope="col" class="tableauSize">Temps</th>
+                        <th scope="col" class="tableauSize fontText">Module</th>
+                        <th scope="col" class="tableauSize fontText">Sujet ciblé</th>
+                        <th scope="col" class="tableauSize fontText">Temps</th>
                     </tr>
                 </thead>
                 <tbody class="table-group-divider">
@@ -124,52 +120,52 @@
         </div>
     </div>
 
-    <div class="text-center">
-        <p>Début des leçons de pilotage pratique a l'issu de l'obention du PPL théorique .</p>
+    <div class="text-center mt-3 fontText">
+        <p>Début des leçons de pilotage pratique a l'issu de l'obention du PPL théorique. avec instructeur.Le planing s'adaptera à vos besoin.</p>
     </div>
 
-    <div class="row m-0 p-0 justify-content-center">
+    <div class="row m-0 p-0 justify-content-center ">
 
-        <div class="col-lg-3 col-11 text-center">
-            7 heures en simulateur de vol
+        <div class="col-lg-3 col-11 text-center Textcolor m-3">
+            <p class="m-2 fontText">7 heures en simulateur de vol.</p>
         </div>
 
-        <div class="col-lg-3 col-11 text-center">
-            28 heures en vol avec instructeur
+        <div class="col-lg-3 col-11 text-center Textcolor m-3">
+            <p class="m-2 fontText">28 heures en vol avec instructeur.</p>
         </div>
 
     </div>
 
 
-    <p class="text-center fs-4">Formulaire</p>
+    <p class="text-center fs-4 textColor2 fontTitle">Formulaire</p>
 
 
     <div class="row m-0 p-0 justify-content-center ">
-        <div class="col-lg-3 border border-dark m-2">
+        <div class="col-lg-3  m-2">
             <div class="input-group flex-nowrap  FormularSize">
-                <span class="input-group-text border border-dark spanSize" id="addon-wrapping">Nom</span>
-                <input type="text" class="form-control border border-dark" aria-label="Username" aria-describedby="addon-wrapping">
+                <span class="input-group-text spanSize fontText" id="addon-wrapping">Nom</span>
+                <input type="text" class="form-control " aria-label="Username" aria-describedby="addon-wrapping">
             </div>
             <div class="input-group flex-nowrap  FormularSize mt-4">
-                <span class="input-group-text border border-dark spanSize" id="addon-wrapping">Prénom</span>
-                <input type="text" class="form-control border border-dark" aria-label="Username" aria-describedby="addon-wrapping">
+                <span class="input-group-text spanSize fontText" id="addon-wrapping">Prénom</span>
+                <input type="text" class="form-control " aria-label="Username" aria-describedby="addon-wrapping">
             </div>
             <div class="input-group flex-nowrap  FormularSize mt-4">
-                <span class="input-group-text border border-dark spanSize" id="addon-wrapping">Email</span>
-                <input type="text" class="form-control border border-dark" aria-label="Username" aria-describedby="addon-wrapping">
+                <span class="input-group-text spanSize fontText" id="addon-wrapping">Email</span>
+                <input type="text" class="form-control " aria-label="Username" aria-describedby="addon-wrapping">
             </div>
             <div class="input-group flex-nowrap  FormularSize mt-4">
-                <span class="input-group-text border border-dark spanSize" id="addon-wrapping">Mobile</span>
-                <input type="text" class="form-control border border-dark" aria-label="Username" aria-describedby="addon-wrapping">
+                <span class="input-group-text spanSize fontText" id="addon-wrapping">Mobile</span>
+                <input type="text" class="form-control " aria-label="Username" aria-describedby="addon-wrapping">
             </div>
         </div>
-        <div class="form-floating col-lg-3 border border-dark m-2">
+        <div class="form-floating col-lg-3 m-2">
             <textarea class=" form-control" style="width: 23rem; height:13.5rem" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-            <label for="floatingTextarea  ">Comments</label>
+            <label for="floatingTextarea  ">Message</label>
         </div>
 
-        <div class=" row d-flex justify-content-center border border-dark mt-3 m-0 p-0">
-            <button type="button" class="btn col-lg-3 btn-primary buttonSize">Envoyer</button>
+        <div class=" row d-flex justify-content-center mt-3 mb-3 m-0 p-0">
+            <button type="button" class="btn col-lg-3 Textcolor buttonSize">Envoyer</button>
         </div>
 
 
